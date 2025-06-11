871aeb694b7f4bf1b1ddf0cb7e4255f767abfdda
Alex <186313861+atbu@users.noreply.github.com>
2025-06-11 20:48:08 +0100

    Create Rockhopper.Git project to contain all interaction with Git. Add functionality to Open button in top menu bar.

---
+++ Rockhopper.Git/RepositoryHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace Rockhopper.Git;
+
+public class RepositoryHelper
+{
+    public static bool DoesGitRepositoryExist(string path)
+    {
+        return Directory.Exists($"{path}\\.git");
+    }
+}
---
+++ Rockhopper.UI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using Rockhopper.Git;
+
+namespace Rockhopper;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public string CurrentRepository { get; set; }
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void RepositoryOpenClicked(object sender, RoutedEventArgs e)
+    {
+        Microsoft.Win32.OpenFolderDialog dialog = new()
+        {
+            Multiselect = false,
+            Title = "Open Repository"
+        };
+
+        bool? result = dialog.ShowDialog();
+
+        string? fullPathToFolder = "";
+        if (result == true)
+        {
+            fullPathToFolder = dialog.FolderName;
+        }
+
+        if (fullPathToFolder != "")
+        {
+            if (RepositoryHelper.DoesGitRepositoryExist(fullPathToFolder))
+            {
+                MessageBox.Show("Git repository exists at this location.");
+            }
+            else
+            {
+                MessageBox.Show("No Git repository exists at this location.");
+            }
+        }
+        else
+        {
+            MessageBox.Show("No folder selected. Cancelling.");
+        }
+        
+    }
+}
