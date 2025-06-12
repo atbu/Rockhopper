178c8b83befb654e390e78d9bf050e38bc623c5a
Alex <186313861+atbu@users.noreply.github.com>
2025-06-12 21:49:45 +0100

    Migrate UI files from Rockhopper namespace to Rockhopper.UI

--- Rockhopper.UI/App.xaml.cs
+++ Rockhopper.UI/App.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Rockhopper;
+namespace Rockhopper.UI;
 
 /// <summary>
 /// Interaction logic for App.xaml
