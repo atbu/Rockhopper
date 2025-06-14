a6c247762f82391ae22820efdc8c3bea002f6076
Alex <186313861+atbu@users.noreply.github.com>
2025-06-14 09:40:55 +0100

    Re-implement opening of repositories and detection of branches - now displays properly in bottom bar

--- Rockhopper.Git/Models/Repository.cs
+++ Rockhopper.Git/Models/Repository.cs
@@ -3,6 +3,7 @@ namespace Rockhopper.Git.Models;
 public class Repository
 {
     public string Path { get; set; }
+    public string RootFolderName { get; set; }
     public string HEAD { get; set; }
     public Branch[] Branches { get; set; }
 
--- Rockhopper.UI/MainWindow.xaml.cs
+++ Rockhopper.UI/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ public partial class MainWindow : Window
 {
     private readonly IRepositoryService _repositoryService;
 
+    public Repository Repository { get; set; }
+    public string RepositoryName { get; set; }
     public string HEAD
     {
         get { return (string)GetValue(HEADProperty); }
@@ -26,7 +28,27 @@ public partial class MainWindow : Window
         
         InitializeComponent();
         this.DataContext = this;
+    }
 
-        HEAD = "Test";
+    private void TopMenuRepositoryOpenClicked(object sender, RoutedEventArgs e)
+    {
+        Microsoft.Win32.OpenFolderDialog dialog = new();
+
+        dialog.Multiselect = false;
+        dialog.Title = "Open Repository";
+
+        bool? result = dialog.ShowDialog();
+
+        if (result == true)
+        {
+            string fullPathToFolder = dialog.FolderName;
+            string folderNameOnly = dialog.SafeFolderName;
+
+            Repository = new Repository(fullPathToFolder);
+            Repository.RootFolderName = folderNameOnly;
+            Repository.HEAD = _repositoryService.GetHEAD(Repository);
+
+            HEAD = Repository.HEAD;
+        }
     }
 }
