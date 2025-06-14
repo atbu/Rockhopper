acc6f8e54b137d9d2ef2057fdf779cc13bc2d624
Alex <186313861+atbu@users.noreply.github.com>
2025-06-14 09:54:59 +0100

    Implement branch checkout dialog and return data from it

--- Rockhopper.UI/MainWindow.xaml.cs
+++ Rockhopper.UI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using Rockhopper.Git.Models;
 using Rockhopper.Git.Services;
+using Rockhopper.UI.Dialogs;
 
 namespace Rockhopper.UI;
 
@@ -12,7 +13,6 @@ public partial class MainWindow : Window
     private readonly IRepositoryService _repositoryService;
 
     public Repository Repository { get; set; }
-    public string RepositoryName { get; set; }
     public string HEAD
     {
         get { return (string)GetValue(HEADProperty); }
@@ -49,6 +49,15 @@ public partial class MainWindow : Window
             Repository.HEAD = _repositoryService.GetHEAD(Repository);
 
             HEAD = Repository.HEAD;
+        }
+    }
+
+    private void TopMenuBranchCheckoutClicked(object sender, RoutedEventArgs e)
+    {
+        var dialog = new BranchCheckoutDialog();
+        if (dialog.ShowDialog() == true)
+        {
+            MessageBox.Show("You're trying to check out " + dialog.BranchToCheckout);
         }
     }
 }
